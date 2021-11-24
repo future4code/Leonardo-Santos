2b672fb214abd26792c538ff113bf8d397c05319
leonardo-silva988 <leonardo.silva988@hotmail.com>
2021-11-24 16:10:24 -0300

    Resolvendo problemas do botão do projeto

---
+++ modulo3/rend-condicional-form/projeto-labenuforms/src/components/Etapa3.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components'
+
+class Etapa3 extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Etapa 3 - INFORMAÇÕES GERAIS DE ENSINO</h1>
+        <h2>5. Por que você não terminou um curso de graduação?</h2>
+        <input />
+        <h2>6. Você fez algum curso complementar?</h2>
+        <select>
+          <option value="valor1">Nenhum</option>
+          <option value="valor1">Curso técnico</option>
+          <option value="valor1">Curso De inglês</option>
+        </select>
+        <br/><br/>
+      </div>
+    )
+  }
+}
+
+export default Etapa3
