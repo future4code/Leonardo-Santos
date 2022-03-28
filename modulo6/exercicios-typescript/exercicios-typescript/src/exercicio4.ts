05d54ce9fb0a1b13ebe915be748df12c922d7c25
leonardo-silva988 <leonardo.silva988@hotmail.com>
2022-03-28 02:45:34 -0300

    realizado o exercicio 4

--- modulo6/exercicios-typescript/exercicios-typescript/src/exercicio4.ts
+++ modulo6/exercicios-typescript/exercicios-typescript/src/exercicio4.ts
@@ -1,17 +1,32 @@
-function comparaDoisNumeros(num1: number, num2: number): number {
-    let maiorNumero
-    let menorNumero
-
-    if (num1 > num2) {
-        maiorNumero = num1
-        menorNumero = num2
-    }else {
-        maiorNumero = num2
-        menorNumero = num1
-    }
-
-    const diferenca = maiorNumero - menorNumero
-
-    return diferenca
+enum SETORES {
+	MARKETING="marketing",
+	VENDAS="vendas",
+	FINANCEIRO="financeiro",
 }
-console.log(comparaDoisNumeros(4,7))
+
+type Pessoas = {
+    nome: string,
+    salario: number,
+    setor: SETORES,
+    presencial: boolean
+}
+
+
+const array = 
+[
+	{ nome: "Marcos", salário: 2500, setor: SETORES.MARKETING, presencial: true },
+	{ nome: "Maria" ,salário: 1500, setor: SETORES.VENDAS, presencial: false},
+	{ nome: "Salete" ,salário: 2200, setor: SETORES.FINANCEIRO, presencial: true},
+	{ nome: "João" ,salário: 2800, setor: SETORES.MARKETING, presencial: false},
+	{ nome: "Josué" ,salário: 5500, setor: SETORES.FINANCEIRO, presencial: true},
+	{ nome: "Natalia" ,salário: 4700, setor: SETORES.VENDAS, presencial: true},
+	{ nome: "Paola" ,salário: 3500, setor: SETORES.MARKETING, presencial: true }
+]
+
+
+const novoArray = array.filter((array) => {
+    return array.presencial === true && array.setor === "marketing"
+})
+
+
+console.log(novoArray)
