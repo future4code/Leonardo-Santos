a587044b16d23ec1519fa3ef87e969306c01abae
leonardo-silva988 <leonardo.silva988@hotmail.com>
2022-05-13 18:14:35 -0300

    resolvendo exercicio 3

--- modulo7/heranca-polimorfismo/src/index.ts
+++ modulo7/heranca-polimorfismo/src/index.ts
@@ -34,4 +34,28 @@ const user: User = new User ("1", "teste@", "jaime", "senha")
 
 // console.log(user)
 
+export class Customer extends User {
+  public purchaseTotal: number = 0;
+  private creditCard: string;
+
+  constructor(
+    id: string,
+    email: string,
+    name: string,
+    password: string,
+    creditCard: string
+  ) {
+    super(id, email, name, password);
+    console.log("Chamando o construtor da classe Customer");
+    this.creditCard = creditCard;
+  }
+
+  public getCreditCard(): string {
+    return this.creditCard;
+  }
+}
+
+const customer: Customer = new Customer ("2", "teste2@", "ronaldo", "senha123", "25")
+console.log(customer);
+
 
