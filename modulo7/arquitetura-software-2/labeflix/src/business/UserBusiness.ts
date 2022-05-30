3d3b5ccd139871165c6eaf25e82b1dbd7d3f4df5
leonardo-silva988 <leonardo.silva988@hotmail.com>
2022-05-30 18:53:35 -0300

    realizado todos exercicios

--- modulo7/arquitetura-software-2/labeflix/src/business/UserBusiness.ts
+++ modulo7/arquitetura-software-2/labeflix/src/business/UserBusiness.ts
@@ -1,21 +1,27 @@
-import { UserDatabase } from "../data/UserDatabase"
-import { v4 as generateId } from 'uuid'
+import { UserDatabase } from "../data/UserDatabase";
+import { v4 as generateId } from "uuid";
+import { User } from "../types/User";
 
+const userDatabase = new UserDatabase();
 export class UserBusiness {
-  async create({ email, name, password }: any):Promise<void> {
+  async create({ email, name, password }: any): Promise<void> {
     if (!email || !name || !password) {
-      throw new Error("Dados inválidos (email, name, password)")
+      throw new Error("Dados inválidos (email, name, password)");
     }
 
-    const id = generateId()
+    const id = generateId();
 
-    const userDatabase = new UserDatabase()
+    
     await userDatabase.create({
       id,
       name,
       email,
-      password
-    })
+      password,
+    });
   }
 
+  async findAll(): Promise<User[]> {
+    const result = await userDatabase.findAll();
+    return result;
+  }
 }
